632c1693030481337ff9beac4ca02fd7010d9017
Mmoya123 <82730331+Mmoya123@users.noreply.github.com>
2022-07-08 21:22:06 -0300

    Create Producto.java

---
+++ Producto.java
@@ -0,0 +1,21 @@
+paquete  supermark_GA10 ;
+
+importar  Java . yo _ IOExcepción ;
+importar  Java . sql . fecha ;
+importar  Java . útil _ escáner ;
+
+ Registro de clase  pública {
+
+	public  static  void  main ( String [] args ) lanza  IOException {
+		
+		CRUDproducto crud = new CRUDproducto ();		
+
+		Fecha  fecha = nueva  Fecha ( 101 , 8 , 28 );
+
+		Usuario  user = new  Usuario ( "Santiago" , "Ruiz" , "ybn3421" , "santiruiz2001@outlook.com" , 43640893 , fecha );{
+
+    }
+  basura_producto (producto);
+  }
+   
+ }
